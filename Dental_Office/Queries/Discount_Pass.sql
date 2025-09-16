b34a3198ee8ca5b78439176d55a27a296ea3234f
Abderrahim Barhoumi <120909154+AbderrahimBarhoumi@users.noreply.github.com>
2025-09-16 17:49:46 +0100

    Discount_Pass.sql

--- Dental_Office/Queries/Discount_Pass.sql
+++ Dental_Office/Queries/Discount_Pass.sql
@@ -1,4 +1,4 @@
---Patients who has the loyalty pass get discount per 10%
+--Patients who have the loyalty pass get a discount of 10%
 UPDATE billing
 SET amount_required = amount_required * 0.9
 WHERE appointment_num IN (
@@ -7,4 +7,5 @@ FROM appointments a
 JOIN patients p ON a.patient_id = p.patient_id
 WHERE p.loyalty_pass = TRUE
 );
+
 
