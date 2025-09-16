c80442ba42d8ea89404ea9d8b4c1409654ea8a25
Abderrahim Barhoumi <120909154+AbderrahimBarhoumi@users.noreply.github.com>
2025-09-16 21:35:15 +0100

    Apply Loyalty_Pass.sql
    Apply Loyalty_Pass function

--- Dental_Office/Functions/Loyalty_Pass.sql
+++ Dental_Office/Functions/Loyalty_Pass.sql
@@ -1,4 +1,4 @@
---Intilaize a function that provides a discount for loyal patients
+--Initialize a function that provides a discount for loyal patients
 
 CREATE OR REPLACE FUNCTION Unlock_Loyalty_Pass()
 RETURNS VOID AS $$
@@ -10,6 +10,24 @@ BEGIN
         FROM appointments a
         JOIN patients p ON a.patient_id = p.patient_id
         WHERE p.loyalty_pass = TRUE
+    );
+END;
+$$ LANGUAGE plpgsql;
+
+
+--Apply and update loyalty_pass function using this query
+
+CREATE OR REPLACE FUNCTION update_loyalty_pass()
+RETURNS VOID AS $$
+BEGIN
+    UPDATE patients
+    SET loyalty_pass = TRUE
+    WHERE patient_id IN (
+        SELECT patient_id
+        FROM appointments
+        WHERE status = 'Completed' --Patients who has completed status 
+        GROUP BY patient_id
+        HAVING COUNT(*) > 5  --and passed to their sixth appointment
     );
 END;
 $$ LANGUAGE plpgsql;
